a4237fbef32726d218c413a969dbcd6383bc11c9
dwiilhammaulana <1123150008@global.ac.id>
2025-12-28 14:22:59 +0700

    membuat spash_screen3 tahap 2 lengkungan diatas

--- lib/page/splash_screen3.dart
+++ lib/page/splash_screen3.dart
@@ -14,3 +14,24 @@ class SplashPageThree extends StatelessWidget {
     );
   }
 }
+
+// ini untuk buat lengkungan item diatas
+class TopCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 60);
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height + 40,
+      size.width,
+      size.height - 60,
+    );
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
