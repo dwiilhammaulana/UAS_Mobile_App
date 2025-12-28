41a59f0243fa92625bc2e72481cb82e7a28a749e
dwiilhammaulana <1123150008@global.ac.id>
2025-12-28 14:17:27 +0700

    membuat spash_screen3 tahap 1

---
+++ lib/page/splash_screen3.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SplashPageThree extends StatelessWidget {
+  const SplashPageThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFC107),
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
