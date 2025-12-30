6871b9b2fbcb5ff0dcb3f87c00f3e65109f2da8e
Ramzscuyyy <1123150076@global.ac.id>
2025-12-30 21:53:23 +0700

    create intro animation page widget

--- lib/page/intro.dart
+++ lib/page/intro.dart
@@ -4,4 +4,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:testing_2/page/home.dart';
 import 'package:testing_2/page/splash_screen1.dart';
+class IntroAnimationPage extends StatefulWidget {
+  const IntroAnimationPage({super.key});
+
+  @override
+  State<IntroAnimationPage> createState() => _IntroAnimationPageState();
+}
 
