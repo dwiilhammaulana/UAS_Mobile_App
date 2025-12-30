2ea841a0aa5ad0a243c97c89a75628de3c905a9a
Lendra <lendradzr77@gmail.com>
2025-12-30 21:02:07 +0700

    custom clipper

--- lib/page/splash_screen1.dart
+++ lib/page/splash_screen1.dart
@@ -129,4 +129,26 @@ class MyWidget extends StatelessWidget {
       ),
     );
   }
+
+  /// ===== CUSTOM CLIPPER UNTUK LENGKUNG =====
+class TopCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 60);
+
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height + 40,
+      size.width,
+      size.height - 60,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
