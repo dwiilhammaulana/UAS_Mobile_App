49cb0aa9a27ab185a65c8372aaf11fdcb37a9166
Ramzscuyyy <1123150076@global.ac.id>
2025-12-30 21:52:28 +0700

    add navigation target pages

--- lib/page/intro.dart
+++ lib/page/intro.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:testing_2/page/home.dart';
+import 'package:testing_2/page/splash_screen1.dart';
 
