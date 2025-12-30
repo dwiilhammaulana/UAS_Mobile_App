bb5d537cb2ed00c275eae7b3620652b03de3163a
dwiilhammaulana <1123150008@global.ac.id>
2025-12-30 22:29:01 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: true,
-      home: IntroAnimationPage(),
+      home: SplashPageOne(),
     );
   }
 }
