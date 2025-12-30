4ba8c52eb72f1bc0a47fbcc35dfef33871933413
Lendra <lendradzr77@gmail.com>
2025-12-30 20:39:14 +0700

    buat coding stl

---
+++ lib/page/splash_screen1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:testing_2/page/splash_screen2.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
