cb0b63a442305b0b835d0ad72dbea5965c5d532d
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 14:25:34 +0700

    Kerangka TodoDetailPage + terima parameter todo

--- lib/page/todo_detail.dart
+++ lib/page/todo_detail.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class TodoDetailPage extends StatelessWidget {
-  const TodoDetailPage({super.key});
+class TodoDetailPage extends StatefulWidget {
+  final Map<String, dynamic> todo;
+
+  const TodoDetailPage({super.key, required this.todo});
 
   @override
+  State<TodoDetailPage> createState() => _TodoDetailPageState();
+}
+
+class _TodoDetailPageState extends State<TodoDetailPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Task Detail"),
+      ),
+      body: const Center(
+        child: Text("Todo Detail Page"),
+      ),
+    );
   }
 }
