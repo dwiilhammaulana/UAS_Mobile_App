d535f60189a8e92a070aa7bf67ccc424f45ad9c2
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 13:08:43 +0700

    import home page helper navigasi ke home

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uas_mobile_app/page/home.dart';
 
 final supabase = Supabase.instance.client;
 
@@ -15,16 +16,19 @@ class _AuthScreenState extends State<AuthScreen> {
   final TextEditingController _passwordController = TextEditingController();
   String _message = "";
 
+  void _navigateToHome() {
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TextField(controller: _emailController),
-          TextField(controller: _passwordController),
-          if (_message.isNotEmpty) Text(_message),
-        ],
-      ),
+    return const Scaffold(
+      body: Center(child: Text("Auth")),
     );
   }
 
