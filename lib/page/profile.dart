5d2bb686c0c215451370296f9a72e9081b4e4432
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 15:25:40 +0700

    Tambah UI formTambah UI form

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -20,8 +20,36 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text("Edit Profil"),
         backgroundColor: const Color(0xFFFFC107),
       ),
-      body: const Center(
-        child: Text("Profile Page"),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          _buildTextField(_fullNameController, "Nama Lengkap", Icons.person),
+          _buildTextField(_usernameController, "Username", Icons.alternate_email),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF111827),
+              foregroundColor: Colors.white,
+              minimumSize: const Size(double.infinity, 55),
+            ),
+            child: const Text("SIMPAN PERUBAHAN"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label, IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon),
+          border: const OutlineInputBorder(),
+        ),
       ),
     );
   }
