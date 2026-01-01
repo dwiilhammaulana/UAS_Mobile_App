6cd8bde26ea037f437aebe055150422d3d69e261
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 14:28:18 +0700

    Tambah Supabase client + intl import

--- lib/page/todo_detail.dart
+++ lib/page/todo_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class TodoDetailPage extends StatefulWidget {
   final Map<String, dynamic> todo;
@@ -10,6 +12,8 @@ class TodoDetailPage extends StatefulWidget {
 }
 
 class _TodoDetailPageState extends State<TodoDetailPage> {
+  final supabase = Supabase.instance.client;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
