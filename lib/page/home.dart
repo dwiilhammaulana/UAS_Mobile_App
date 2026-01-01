ddfa852aff8faec382e2d970bfca5662d381cd90
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 14:13:56 +0700

    buat model todo

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:uas_mobile_app/page/todo_detail.dart';
 
 
 class HomePage extends StatefulWidget {
