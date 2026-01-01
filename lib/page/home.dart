6331200470fecf5d0c76cb9cf13f2cb50b14fe75
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 13:56:01 +0700

    Tambah Supabase client

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final supabase = Supabase.instance.client;
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
