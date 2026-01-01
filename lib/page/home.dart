cb10c191c1bded6174b335df9bfacecdcc12fc61
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 13:59:35 +0700

    InitState: aktifkan stream todos

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -23,6 +23,15 @@ class _HomePageState extends State<HomePage> {
   late final Stream<List<Map<String, dynamic>>> _todoStream;
 
   @override
+  void initState() {
+    super.initState();
+    _todoStream = supabase
+        .from('todos')
+        .stream(primaryKey: ['id'])
+        .order('created_at', ascending: false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(child: Text("Home")),
