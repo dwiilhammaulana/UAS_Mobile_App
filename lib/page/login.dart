7150380d67733d828a7ad9f9ba534c491184fc6e
dwiilhammaulana <1123150008@global.ac.id>
2026-01-01 13:00:06 +0700

    bikin kerangka page login dan akses client Supabase

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class AuthScreen extends StatelessWidget {
+final supabase = Supabase.instance.client;
+
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(child: Text("Auth Screen")),
+    );
   }
 }
