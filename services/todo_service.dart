3ba55d71d7f3585b9ca711626d7eb5a283499ca8
dwiilhammaulana <1123150008@global.ac.id>
2026-01-02 22:21:44 +0700

    Tambah fetchTodos()

--- services/todo_service.dart
+++ services/todo_service.dart
@@ -3,4 +3,13 @@ import '../lib/models/todo_model.dart';
 
 class TodoService {
   final supabase = Supabase.instance.client;
+
+  Future<List<dynamic>> fetchTodosRaw() async {
+    final response = await supabase
+        .from('todos')
+        .select()
+        .order('created_at', ascending: false);
+
+    return response as List<dynamic>;
+  }
 }
