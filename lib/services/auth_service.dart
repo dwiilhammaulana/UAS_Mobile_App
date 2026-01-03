ccd1ca91322552856c059da8a4e52e68552174c6
dwiilhammaulana <1123150008@global.ac.id>
2026-01-04 02:10:40 +0700

    Trigger pemilihan akun Google + handle cancel

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -6,6 +6,9 @@ class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<UserCredential?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    if (googleUser == null) return null;
+
     return null;
   }
 }
