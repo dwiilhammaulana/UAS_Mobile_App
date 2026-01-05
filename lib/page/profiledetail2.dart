fc2f137a23c6f90de9d87e6a704ab997742091c8
Ramzscuyyy <1123150076@global.ac.id>
2026-01-05 15:34:07 +0700

    use scaffold as base layout

--- lib/page/profiledetail2.dart
+++ lib/page/profiledetail2.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 class ProfileDetailPage extends StatelessWidget {
   const ProfileDetailPage({super.key});
 
+  static const String nama = "Muhammad Ramzy Hidayat Siregar";
+  static const String nim = "1123150076";
+  static const String kelas = "TI-23-M-SE";
+
+  static const String fotoUrl =
+      "https://ui-avatars.com/api/?name=Muhammad+Ramzy+Hidayat+Siregar&background=1E3A8A&color=FFFFFF&size=256";
+
+  static const List<String> keahlian = [
+    "Flutter Basic",
+    "Firebase",
+    "UI / UX Design",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: const Color(0xFFF1F5F9),
+  );
 }
