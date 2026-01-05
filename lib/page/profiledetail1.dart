9ff501f5869b6023e0def74a9b0ab0cfb2e38f27
Lendra <lendradzr77@gmail.com>
2026-01-05 21:13:16 +0700

    setting data

--- lib/page/profiledetail1.dart
+++ lib/page/profiledetail1.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+class profiledetail1 extends StatelessWidget {
+  final String nama = 'Syailendra Fas Faye';
+  final String nim = '1123150198';
+  final String kelas = 'TI 23 SE M';
+  final String fotoUrl = 'https://i.imgur.com/mwUEjfB.jpeg'; // Placeholder, ganti dengan URL atau asset lokal
+  final List<String> keahlian = ['Flutter Basic', 'Database', 'Administrasi'];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
