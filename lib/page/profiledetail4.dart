01c438403261c9a118ebf64424f39be2cba203af
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2026-01-12 23:18:55 +0700

    add static skill list for profile

--- lib/page/profiledetail4.dart
+++ lib/page/profiledetail4.dart
@@ -7,6 +7,12 @@ class Profiledetail4 extends StatelessWidget {
   static const String nim = "1123150002";
   static const String kelas = "TI-3A";
 
+  static const List<String> skillList = [
+    "Cyber security",
+    "AI ENGGINER",
+    "Backend Developer",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
