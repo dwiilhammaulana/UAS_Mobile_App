bdd630a5ca1c026794a118a9efa6a3480f5f3350
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2026-01-12 23:15:09 +0700

    add static constants for  name

--- lib/page/profiledetail4.dart
+++ lib/page/profiledetail4.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class Profiledetail4 extends StatelessWidget {
   const Profiledetail4({super.key});
 
+  static const String nama = "Muhamad Ulin Nuha";
+ 
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
