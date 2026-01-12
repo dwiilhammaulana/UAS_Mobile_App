60b8ddce2880a4d1e71a0a589eafb64984a0b536
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2026-01-12 23:16:11 +0700

    add static constants for  nim

--- lib/page/profiledetail4.dart
+++ lib/page/profiledetail4.dart
@@ -4,6 +4,7 @@ class Profiledetail4 extends StatelessWidget {
   const Profiledetail4({super.key});
 
   static const String nama = "Muhamad Ulin Nuha";
+  static const String nim = "1123150002";
  
 
   @override
