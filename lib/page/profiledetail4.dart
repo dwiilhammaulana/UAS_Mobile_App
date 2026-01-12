5ada4316a9f45dee2402d98872eaa16101f03027
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2026-01-12 23:16:56 +0700

    add static constants for  kelas

--- lib/page/profiledetail4.dart
+++ lib/page/profiledetail4.dart
@@ -5,7 +5,7 @@ class Profiledetail4 extends StatelessWidget {
 
   static const String nama = "Muhamad Ulin Nuha";
   static const String nim = "1123150002";
- 
+  static const String kelas = "TI-3A";
 
   @override
   Widget build(BuildContext context) {
